7b1521624f5330f555371c9d49987491d27e5071
Andrew Halper <ashalper@usgs.gov>
2021-08-24 15:57:57 -0700

    Initial version.

---
+++ .github/workflows/build.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+#
+# Build script for gsflow_v2. Presently a stub.
+#
+
+echo "build.sh called"
---
+++ .github/workflows/setup.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+#
+# Setup script for gsflow_v2. Presently a stub.
+#
+
+echo "setup.sh called"
